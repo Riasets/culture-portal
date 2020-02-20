e5487f96233b8e7d3853208432eb9f399e653782
Riasets <alextel2207@gmail.com>
2020-02-20 19:40:02 +0300

    add text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,58 @@
 import React from "react"
-import { Link, StaticQuery, graphql } from "gatsby"
+import { Link, useStaticQuery, StaticQuery, graphql } from "gatsby"
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <StaticQuery query={graphql`{
-        contentfulTitle(node_locale: {eq: "be-BY"}) {
-          title
+const langs = ['ru', 'by', 'en'];
+
+
+
+
+const IndexPage = ({data}) => {
+
+  return(
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <p>{data.contentfulLongText.by.content[0].content[0].value}</p>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
+
+export const data = graphql`query TextQuery{
+         contentfulLongText {
+          ru {
+            content {
+              content {
+                value
+              }
+            }
+          }
+          en {
+            content {
+              content {
+                value
+              }
+            }
+          }
+          by {
+            content {
+              content {
+                value
+              }
+            }
+          }
         }
-      }`
-    }
-     render={({
-       contentfulTitle: {
-         title
-       }
-     }) => (<p>{title}</p>)}
-                 />
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+      }`;
 
 export default IndexPage
