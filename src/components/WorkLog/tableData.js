a68d2343872be6505b6e178872ad6efc14ea15f5
aplefull <mraplefull@yandex.by>
2020-02-26 00:34:34 +0300

    feat: add file with worklog data

---
+++ src/components/WorkLog/tableData.js
@@ -0,0 +1,33 @@
+function createData(description, hours) {
+    return { description, hours };
+}
+  
+const elaray = [
+    createData('Add custom button', '1h'),
+    createData('Add icons and main colors', '0.5h'),
+    createData('Yeet', '120h'),
+];
+
+const aplefull = [
+    createData('Add custom button', '1h'),
+    createData('Add icons and main colors', '0.5h'),
+    createData('Yeet', '12h'),
+];
+
+const dexnkt = [
+    createData('Add custom button', '1h'),
+    createData('Add icons and main colors', '0.5h'),
+    createData('Watching hentai', '72h'),
+];
+
+const anastasiaKrbvsk = [
+    createData('Add custom button', '1h'),
+    createData('Add icons and main colors', '0.5h'),
+];
+
+const riasets = [
+    createData('Add custom button', '1h'),
+    createData('Add icons and main colors', '0.5h'),
+];
+
+export {elaray, aplefull, dexnkt, anastasiaKrbvsk, riasets}
