de8fbdf935ab2e2fde590fb8be145d8b815f2bcf
Dexnkt001 <dekhand00@mail.ru>
2020-02-29 09:18:12 +0300

    feat:add parallax

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link, useStaticQuery, StaticQuery, graphql } from "gatsby"
 import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
+import { makeStyles } from '@material-ui/core/styles';
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -9,22 +10,34 @@ import SEO from "../components/seo"
 
 const langs = ['ru', 'by', 'en'];
 
-const IndexPage = ({data}) => {
+const useStyles = makeStyles({
+  scane:{
+marginTop:'20px',
+  },
+h1:{
+  color:'white',
+  textAlign:'center',
+},
+h2:{
+  color:'white',
+  textAlign:'center',
+}
+});
 
+
+function parallax(e){
+  this.querySelector('body').style.transform = `translate(${e.clientX/100}px, ${e.clientY/200}px)`
+  this.querySelector('head').style.transform = `translate(${e.clientX/100}px ,${e.clientY/200}px)`
+}
+
+const IndexPage = () => {
+const classes = useStyles ()
   return(
-    <Layout>
-      <SEO title="Home" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
+    document.addEventListener('mousemove', parallax),
+    <div className={classes.scane}>
+      <h1 className={classes.h1}>Welcome to our Belarusian cultural portal:</h1> 
+      <h2 className={classes.h2}>here you can find out a lot of interesting facts about belarusian writers</h2>
       </div>
-      <p>{data.contentfulLongText.by.content[0].content[0].value}</p>
-      <Link to="/page-2/">Go to page 2</Link>
-      <br />
-      <Link to="/styleGuide/">Go to style guide</Link>
-    </Layout>
   )
 }
 
